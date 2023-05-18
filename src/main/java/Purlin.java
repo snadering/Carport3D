22c103d9daa69b3bd810d6812a9154ded0de6351
snadering <cph-sr319@cphbusiness.dk>
2023-05-18 12:12:41 +0200

    purling and rafters ready

--- src/main/java/Purlin.java
+++ src/main/java/Purlin.java
@@ -8,7 +8,12 @@ public class Purlin extends Material{
 
     @Override
     public Geometry3D create(JavaCSG csg) {
-        return csg.box3D(getWidth(), getLength(), getHeight(), false);
+        var purlin = csg.box3D(getWidth(), getLength(), getHeight(), false);
+        var wall1 = csg.box3D(10, 2, getHeight(), false);
+        var wall2 = csg.box3D(10, 2, getHeight(), false);
+        wall1 = csg.translate3D(0, getLength()/2, 0).transform(wall1);
+        wall2 = csg.translate3D(0, -(getLength()/2), 0).transform(wall2);
+        return csg.union3D(purlin, wall1, wall2);
     }
 
 }
--- src/main/java/Rafter.java
+++ src/main/java/Rafter.java
@@ -8,6 +8,12 @@ public class Rafter extends Material{
 
     @Override
     public Geometry3D create(JavaCSG csg) {
-        return csg.box3D(getWidth(), getLength(), getHeight(), false);
+       var rafter = csg.box3D(getWidth(), getLength()+10, getHeight(), false);
+       var cut1 = csg.box3D(getWidth()+2, 6, 11, false);
+       var cut2 = csg.box3D(getWidth()+2, 6, 11, false);
+       cut1 = csg.translate3D(0, getLength()/2-10, -1).transform(cut1);
+       cut2 = csg.translate3D(0, -(getLength()/2-10), -1).transform(cut2);
+
+       return csg.difference3D(rafter, cut1, cut2);
     }
 }
